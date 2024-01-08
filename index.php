1694574249c53eb4101597a19a51a6135dbd8ef1
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-01-08 16:42:30 +0100

    feat: form

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,5 +7,25 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+    <!-- form -->
+    <form action="processo.php" method="POST">
+
+        <!-- area del testo -->
+        <div>
+            <label for="paragraph"> inserisci il testo qui:</label> <br>
+            <textarea name="paragraph" cols="30" rows="10"></textarea>
+        </div>
+        <!-- /area del testo -->
+
+        <!-- area dichiarazione bad word -->
+        <div>
+            <label for="badWord">inserisci qui la parola da censurare</label> <br>
+            <input type="text" name="badWord">
+            <button>invia</button>
+        </div>
+        <!-- /area dichiarazione bad word -->
+        
+    </form>
+    <!-- /form -->
 </body>
 </html>
