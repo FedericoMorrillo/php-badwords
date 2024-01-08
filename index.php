d1eaec5b8949194e47536cbf98dd47bfddc7e1fe
FedericoMorrillo <federicomorrillo98@gmail.com>
2024-01-08 17:39:03 +0100

    feat: css delle pagine

--- index.php
+++ index.php
@@ -4,28 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>PHP Badwords</title>
 </head>
 <body>
-    <!-- form -->
-    <form action="processo.php" method="POST">
+    <div class="container radius">
+        <!-- form -->
+        <form action="processo.php" method="POST" class="flex gap">
 
-        <!-- area del testo -->
-        <div>
-            <label for="paragraph"> inserisci il testo qui:</label> <br>
-            <textarea name="paragraph" cols="30" rows="10"></textarea>
-        </div>
-        <!-- /area del testo -->
+            <!-- area del testo -->
+            <div>
+                <label for="paragraph"><h2>inserisci il testo qui:</h2></label> <br>
+                <textarea name="paragraph" cols="30" rows="10"></textarea>
+            </div>
+            <!-- /area del testo -->
 
-        <!-- area dichiarazione bad word -->
-        <div>
-            <label for="badWord">inserisci qui la parola da censurare</label> <br>
-            <input type="text" name="badWord">
-            <button>invia</button>
-        </div>
-        <!-- /area dichiarazione bad word -->
-        
-    </form>
-    <!-- /form -->
+            <!-- area dichiarazione bad word -->
+            <div>
+                <label for="badWord"><h2>inserisci qui la parola da censurare</h2></label> <br>
+                <input type="text" name="badWord">
+                <button>invia</button>
+            </div>
+            <!-- /area dichiarazione bad word -->
+            
+        </form>
+        <!-- /form -->
+    </div>
 </body>
 </html>
